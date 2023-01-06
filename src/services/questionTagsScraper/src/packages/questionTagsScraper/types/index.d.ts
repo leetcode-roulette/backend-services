c392adfd8d5e76297b5c346d4c3b6af89eff3bdd
Eric <eric.hicks.8989@gmail.com>
2023-01-06 00:01:08 -0800

    Documentation: Add docstrings to `questionTagsScraper` interfaces.

--- src/services/questionTagsScraper/src/packages/questionTagsScraper/types/index.d.ts
+++ src/services/questionTagsScraper/src/packages/questionTagsScraper/types/index.d.ts
@@ -1,9 +1,33 @@
+/**
+ * Leetcode graphQL response for retrieving questions.
+ */
 export interface QuestionData {
+
+  /**
+   * Number value representing a questions id.
+   */
   questionId: number;
+
+  /**
+   * Object array containing a questions tag names.
+   */
   tags: Array<{ slug: string }>;
+
 }
 
+/**
+ * Parsed Leetcode graphql response.
+ */
 export interface QuestionTag {
+
+  /**
+   * Number value representing a questions id.
+   */
   questionId: number;
+
+  /**
+   * String value representing a tag slug.
+   */
   tagSlug: string;
+  
 }
